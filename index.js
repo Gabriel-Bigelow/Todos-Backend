3393ee5e11b44afd42ad16b26ca4898c821f702b
Gabriel-Bigelow <gabebigelow@gmail.com>
2023-02-02 17:17:27 -0500

    add test paths

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 8000;
 // route middlewares
 app.use('/api', todoRoutes);
 
+
+//netlify test route
+app.get('/', (res) => {
+  res.status(200).send('This is working at the root');
+})
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening at http://localhost:${port}`);
--- routes/todo.js
+++ routes/todo.js
@@ -6,4 +6,9 @@ router.post('/todos/create', create);
 router.get('/todos', read);
 router.delete('/todos/:id', removeTodo);
 
+//netlify test route
+router.get('/', (res) => {
+    res.status(200).send('This is working.');
+})
+
 module.exports = router;
