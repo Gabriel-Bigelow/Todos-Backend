bc31ae4eb242d1ea3818d3552acb33ff17ec244f
Gabriel-Bigelow <gabebigelow@gmail.com>
2023-02-03 00:04:49 -0500

    add cors to API router

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // route middlewares
-app.use('/api', todoRoutes);
+app.use('/api', cors(), todoRoutes);
 app.use(cors());
 
 app.get('/test', (req, res) => {
